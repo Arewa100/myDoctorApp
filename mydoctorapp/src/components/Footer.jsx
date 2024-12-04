a658fbf811779bcbe83091eb7f998b40334dcb75
Arewa100 <olasorexway@gmail.com>
2024-12-04 15:27:00 +0100

    update

--- mydoctorapp/src/components/Footer.jsx
+++ mydoctorapp/src/components/Footer.jsx
@@ -8,34 +8,36 @@ const Footer = ()=> {
     return(
         <>
         <div className={style.footer}>
-            <div className={style.outerDiv}>
-                <div className={style.whoWeAreDiv}>
-                    <p>WHO WE ARE</p>
-                    <p>PARTNERS</p>
-                    <p>LEGAL & PRIVACY</p>
-                </div>
-            </div>
-
-            <div className={style.aboutUsDiv}>
-                <p></p><a href="#" className={style.about}>About Us</a>
-                <a href="#" className={style.partner}>Become a partner</a>
-                <a href="#" className={style.privacy}>Privacy policy & terms of use</a>
-            </div>
-
-            <div className={style.logoDiv}>
-                <div className={style.logo}>
-                    <Logo/>
-                    <div className={style.theIcons}>
-                         <div className={style.iconsDiv}>
-                            <img src={linkenin} alt="instagram icon" />
-                            <img src={facebook} alt="facebook icon" />
-                            <img src={instagram} alt="instagram icon" />
-                            <img src={youtube} alt="" />
-                         </div>
+            <div className={style.externalDiv}>
+                <div className={style.outerDiv}>
+                    <div className={style.whoWeAreDiv}>
+                        <p>WHO WE ARE</p>
+                        <p>PARTNERS</p>
+                        <p>LEGAL & PRIVACY</p>
                     </div>
                 </div>
+
+                <div className={style.aboutUsDiv}>
+                    <a href="#" className={style.about}>About Us</a>
+                    <a href="#" className={style.partner}>Become a partner</a>
+                    <a href="#" className={style.privacy}>Privacy policy & terms of use</a>
+                </div>
+    
+                <div className={style.logoDiv}>
+                    <div className={style.logo}>
+                        <Logo/>
+                        <div className={style.theIcons}>
+                            <div className={style.iconsDiv}>
+                                <img src={linkenin} alt="instagram icon" />
+                                <img src={facebook} alt="facebook icon" />
+                                <img src={instagram} alt="instagram icon" />
+                                <img src={youtube} alt="" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
             </div>
-            
         </div>
         </>
     )
